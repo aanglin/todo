de5c8a224edace8dbc31e93002ea86fa07874455
Aaron Anglin <aaron.anglin101@gmail.com>
2023-04-11 17:25:52 -0500

    creating functionality

--- src/pages/api/task/index.js
+++ src/pages/api/task/index.js
@@ -1,76 +1,79 @@
-import { Task } from "../../../models/tasks";
-import { dbConnect } from "@/utils/dbConnect";
+import Task from '../../../models/tasks'
+import dbConnect from '@/utils/dbConnect'
 
-export default async (req, res) => {
-  const { method } = req;
+export default async (req,res) => {
+const {method} = req;
 
-  try {
-    await dbConnect();
-  } catch (error) {
-    console.error("Error connecting to database:", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
-    return;
-  }
+// connect to database
+await dbConnect();
 
-  switch (method) {
-    case "POST":
-      try {
+// create task
+if (method === 'POST') {
+    try {
         const newTask = await new Task(req.body).save();
-        res
-          .status(201)
-          .json({ data: newTask, message: "Task created successfully" });
-      } catch (err) {
-        res.status(500).json({ message: "Internal Server Error" });
-        console.error(err);
-      }
-      break;
+        res.status(201).json({data:newTask, message: "Task created successfully"})
+    }catch (err) {
+        res.status(500).json({message: "Internal Server Error"});
+        console.log(err);
+    }
+}
 
-    case "GET":
-      try {
+// Get all tasks
+if (method === 'GET') {
+    try {
         const tasks = await Task.find();
-        res.status(200).json({ data: tasks });
-      } catch (err) {
-        res.status(500).json({ message: "Internal Server Error" });
-        console.error(err);
-      }
-      break;
-
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).json({ message: `Method ${method} Not Allowed` });
-  }
-};
+        res.status(200).json({data:tasks});
+    }catch (err) {
+        res.status(500).json({message: "Internal Server Error"});
+        console.log(err);
+}
+}
+}
 
 
 
-// import Task from '../../../models/tasks'
-// import dbConnect from '@/utils/dbConnect'
+// import { Task } from "../../../models/tasks";
+// import { dbConnect } from "@/utils/dbConnect";
 
-// export default async (req,res) => {
-// const {method} = req;
+// export default async (req, res) => {
+//   const { method } = req;
 
-// // connect to database
-// await dbConnect();
+//   try {
+//     await dbConnect();
+//   } catch (error) {
+//     console.error("Error connecting to database:", error.message);
+//     res.status(500).json({ message: "Internal Server Error" });
+//     return;
+//   }
 
-// // create task
-// if (method === 'POST') {
-//     try {
+//   switch (method) {
+//     case "POST":
+//       try {
 //         const newTask = await new Task(req.body).save();
-//         res.status(201).json({data:newTask, message: "Task created successfully"})
-//     }catch (err) {
-//         res.status(500).json({message: "Internal Server Error"});
-//         console.log(err);
-//     }
-// }
+//         res
+//           .status(201)
+//           .json({ data: newTask, message: "Task created successfully" });
+//       } catch (err) {
+//         res.status(500).json({ message: "Internal Server Error" });
+//         console.error(err);
+//       }
+//       break;
 
-// // Get all tasks
-// if (method === 'GET') {
-//     try {
+//     case "GET":
+//       try {
 //         const tasks = await Task.find();
-//         res.status(200).json({data:tasks});
-//     }catch (err) {
-//         res.status(500).json({message: "Internal Server Error"});
-//         console.log(err);
-// }
-// }
-// }
+//         res.status(200).json({ data: tasks });
+//       } catch (err) {
+//         res.status(500).json({ message: "Internal Server Error" });
+//         console.error(err);
+//       }
+//       break;
+
+//     default:
+//       res.setHeader("Allow", ["GET", "POST"]);
+//       res.status(405).json({ message: `Method ${method} Not Allowed` });
+//   }
+// };
+
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,112 @@
 import { useState } from 'react'
+import axios from 'axios'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Banner from '../../public/assets/Banner.jpg'
 
+const url = "http://localhost:3000/api/task";
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState({task: ""});
+export default function Home(props) {
+  console.log(props)
+  const [tasks, setTasks] = useState(props.tasks);
+  const [task, setTask] = useState({task: ''});
+
+  const handleChange = ({currentTarget: input}) => {
+    input.value === ""
+    ? setTask({task: ""})
+    : setTask((prev)=> ({...prev, task: input.value}))
+  }
+
+  const editTask = (id) => {
+    const currentTask = tasks.filter((task) => task.id === id);
+    setTask(currentTask[0]);
+  }
+
+  const addTask = async (e) => {
+    e.preventDefault();
+    try {
+      if (task._id){
+        const {data} = await axios.put(url + "/" + task._id,{task: task.task});
+        const originalTasks = [...tasks];
+        const index = originalTasks.findIndex((t) => t.id === task._id);
+        originalTasks[index] = data.data;
+        setTasks(originalTasks);
+        setTask({task: ""});
+      }else {
+        const {data} = await axios.post(url,task);
+        setTasks((prev) => [...prev, data.data]);
+        setTask({task: ""});
+      }
+    }catch (err) {
+     console.error(err);
+    }
+  }
+
+  const updateTask = async (id) => {
+    try {
+      const originalTasks = [...tasks];
+        const index = originalTasks.findIndex((t) => t.id === task._id);
+        const {data} = await axios.put(url + "/" + id,{completed: !originalTasks[index].completed});
+        originalTasks[index] = data.data;
+        setTasks(originalTasks);
+    }catch (err) {
+      console.log(err);
+    }
+  }
+
+  const deleteTask = async (id) => {
+  try {
+    const {data} = await axios.delete(url + "/" + id);
+    setTasks((prev) => prev.filter((task) => task._id !== id));
+  }catch (err) {
+    console.log(err);
+  }
+  }
   return (
   <div id='container' >
     <div id='background-container' className='relative'>
       <div className='absolute inset-0 bg-black/60 z-10'></div>
-      <Image src={Banner} className='object-cover h-[100vh]' /> 
+      <Image src={Banner} className='object-cover h-[100vh]' alt='/' /> 
       </div>
       <div
        id='form-container'
         className='absolute flex flex-col justify-center items-center w-full max-w-md top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20'>
         <h1 className='text-white text-5xl p-8'>ToDo List</h1>
-        <div className='bg-transparent shadow-blue-400 shadow-2xl p-9 rounded-xl w-full text-2xl'>
+        <div className='bg-transparent border-blue-400 border-2 shadow-blue-400 shadow-2xl p-9 rounded-xl w-full text-2xl'>
         <div className='md:p-3'>
-          <form>
+          <form
+          onSubmit={addTask}
+          >
             <input 
             type="text"
             className='w-3/4 bg-transparent border border-r-0 border-blue-400 p-1 focus:outline-none'
             placeholder='Task to do!'
+            onChange={handleChange}
             value={task.task}
              />
              <button
              type='submit'
              className='p-1 pr-2 border border-l-0 border-blue-400 '
              >
-              {task._id ? "Update":"Add"}
+              {tasks._id ? "Update":"Add"}
               </button>
           </form>
         </div>
         <div>
-          {Array.from(task).map((task) => (
+          {tasks.map((task) => (
             <div key={task._id}>
               <input
                type="checkbox"
                checked={task.completed}
+               onChange={() => updateTask(task._id)}
                 />
                 <p className={task.completed}>
                   {task.task}
                 </p>
-                <button>&#9998</button>
-                <button>&#10006</button>
+                <button onClick={() => editTask(task._id)}>&#9998;</button>
+                <button onClick={() => deleteTask(task._id)}>&#10006;</button>
             </div>
           ))}
           {tasks.length === 0 && <h2 className='flex justify-center items-center'>No Tasks</h2>}
@@ -55,4 +115,13 @@ export default function Home() {
       </div>
   </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(url);
+  return {
+    props: {
+      tasks: data.data
+    }
+  };
 }
